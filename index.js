89c5d901f0f66a5ac433e07707c611aed2732f80
educg550 <eduardo9cruz@gmail.com>
2023-02-20 15:44:05 -0300

    First assignment finished

--- index.js
+++ index.js
@@ -1,60 +1,9 @@
 // HTTP global module
 const http = require("http");
-// Filesystem global module
-const fs = require("fs");
+// Function exported in module.exports
+const { routes } = require("./routes");
 
-// Runs given function for every request to http server
-const server = http.createServer((req, res) => {
-  // req.url, req.method and req.headers are the most important atributes from the http request
-
-  // Browsers only know a limited set of headers and values: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
-  res.setHeader("Content-Type", "text/html");
-
-  if (req.url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    // name attribute is important to the server request
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (req.url === "/message" && req.method === "POST") {
-    const body = [];
-    // Executed when new chunk is received by server
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    // Executed when all chunks are done. This will be executed AFTER NodeJS finishes parsing the request
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-
-      // Async func that gets callback, avoiding blocking rest of the code execution
-      fs.writeFile(".cache/data.txt", message, (err) => {
-        if (err) return console.log(err);
-        // 302: Temporarily moved
-        res.statusCode = 302;
-        // Redirect by setting header location
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-
-  // Ends the event loop (not used often in real environments)
-  // process.exit();
-
-  // Tells browser that server finished writing onto it
-  res.end();
-});
+// Runs given function for every request to http server. It also parses req and res params through shorthanded syntax
+const server = http.createServer(routes);
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,63 @@
+// Filesystem global module
+const fs = require("fs");
+
+// req.url, req.method and req.headers are the most important atributes from the http request
+const handleRequests = (req, res) => {
+  // Browsers only know a limited set of headers and values: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+  res.setHeader("Content-Type", "text/html");
+
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<head><title>First NodeJS Assignment</title><head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1>");
+    res.write("<h2>Enter Username</h2>");
+    // name attribute is important to the server request
+    res.write(
+      '<form action="/create-user" method="POST"><input type="text" name="user"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (req.url === "/create-user" && req.method === "POST") {
+    const body = [];
+    // Executed when new chunk is received by server. Manually gets all the data sent alongside within the POST request
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    // Executed when all chunks are done. This will be executed AFTER NodeJS finishes parsing the request. IT IS IMPORTANT TO RETURN THE RESULT OF THE FINAL req.on call
+    return req.on("end", () => {
+      // Manually parses all chunks raw content
+      const parsedBody = Buffer.concat(body).toString();
+      const username = parsedBody.split("=")[1];
+
+      console.log({ username });
+
+      res.setHeader("Location", "/");
+      res.statusCode = 302;
+      return res.end();
+    });
+  }
+
+  if (req.url === "/users" && req.method === "GET") {
+    res.write("<html>");
+    res.write("<head><title>User List</title><head>");
+    res.write("<body><h1>Registered users</h1>");
+    res.write("<ul><li>User 1</li>");
+    res.write("<li>User 2</li>");
+    res.write("<li>User 3</li>");
+    res.write("<li>User 4</li>");
+    res.write("<li>User 5</li></ul>");
+    res.write("</html>");
+  }
+
+  // Ends the event loop (not used often in real environments)
+  // process.exit();
+
+  // Tells browser that server finished writing onto it
+  res.end();
+};
+
+// Shortcut so we can export several values through an exported JS object
+exports.routes = handleRequests;
