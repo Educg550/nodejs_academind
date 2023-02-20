229ffbdf7d3553a5d1ed98c82db2b138c81f99e6
educg550 <eduardo9cruz@gmail.com>
2023-02-20 15:04:41 -0300

    Added endpoint default template

--- index.js
+++ index.js
@@ -27,18 +27,28 @@ const server = http.createServer((req, res) => {
     req.on("data", (chunk) => {
       body.push(chunk);
     });
-    // Executed when all chunks are done
-    req.on("end", () => {
+
+    // Executed when all chunks are done. This will be executed AFTER NodeJS finishes parsing the request
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      fs.writeFileSync(".cache/data.txt", message);
+
+      // Async func that gets callback, avoiding blocking rest of the code execution
+      fs.writeFile(".cache/data.txt", message, (err) => {
+        if (err) return console.log(err);
+        // 302: Temporarily moved
+        res.statusCode = 302;
+        // Redirect by setting header location
+        res.setHeader("Location", "/");
+        return res.end();
+      });
     });
-    // 302: Temporarily moved
-    res.statusCode = 302;
-    // Redirect by setting header location
-    res.setHeader("Location", "/");
-    return res.end();
   }
+
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
 
   // Ends the event loop (not used often in real environments)
   // process.exit();
