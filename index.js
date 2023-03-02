8d405c2ed24f4bd01ca969e50a4d369e7bcbd2ca
educg550 <eduardo9cruz@gmail.com>
2023-03-02 15:14:07 -0300

    Example assignment done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const app = express();
+
+app.use("/users", (req, res) => {
+  console.log(`Now on route ${req.headers.location}`);
+  res.sendFile(__dirname + "/views/users.html");
+});
+
+app.use("/", (req, res) => {
+  console.log(`Now on route ${req.headers.location}`);
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.listen(3000);
